5755ffd6d3ec03f88fb813f57c4676ef67cdc9c5
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-07-23 19:48:44 -0400

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        array = []
+        for i in range(len(nums)):
+            sub_array = []
+            product = 1
+            for j in range(len(nums)):
+                if j != i:
+                    # sub_array.append(num)
+                    product *= nums[j]
+            array.append(product)
+            # print(f"sub_array = {sub_array}")
+            # print(f"  array = {array}")
+        return array
+        # for a in array:
+
+# Time O(n^2)
+# Space O(n)
