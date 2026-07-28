de8f705ac4d7674f29467fe4c07014e59fb097a9
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-07-28 19:36:16 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # s2 = s.replace(" ", "").lower()
+        # print(s2)
+        s2 = ""
+        for char in s:
+            if char.isalnum():
+                # s2.add(char.lower())
+                s2 += char.lower()
+        # print(s2)
+        return s2 == s2[::-1]
