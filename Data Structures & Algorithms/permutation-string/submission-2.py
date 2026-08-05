a9974262690261a9fabf1514aba254396b22a728
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-08-05 19:40:50 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        target = Counter(s1)
+        # print(f"target = {target}")
+        window = Counter()
+        l = 0
+        for r in range(len(s2)):
+            window[s2[r]] += 1  # Add new char
+            if r - l + 1 > len(s1):
+                window[s2[l]] -= 1  # Remove left char
+                l += 1  # Next char
+            if window == target:
+                return True
+        return False
+
+# s1 = "abc", s2 = "lecabee"
+#                    l
+#                    r
+# window[s2[r]] -> {"l": 1, "e": 1}
+# r - l + 1 = 1
