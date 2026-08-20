621445eb171edf6abdc7fead84b75199e9dddbca
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-08-20 19:12:48 -0400

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        firsts = [x[0] for x in matrix]
+        # print(f"firsts = {firsts}")
+
+        l, r = 0, len(firsts) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            curr = firsts[mid]
+            if curr < target:
+                l = mid + 1
+            elif curr > target:
+                r = mid - 1
+            else:
+                return True
+        # print(f"l, r = {l, r}")  # r has target
+
+        l2, r2 = 0, len(matrix[0]) - 1
+        while l2 <= r2:
+            mid2 = (l2 + r2) // 2
+            curr2 = matrix[r][mid2]
+            if curr2 < target:
+                l2 = mid2 + 1
+            elif curr2 > target:
+                r2 = mid2 - 1
+            else:
+                return True
+
+        return False
+            
+# Time O(log m + log n)                
+# Space O(m)
